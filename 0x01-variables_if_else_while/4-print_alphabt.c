61b70b056247c2c0d0d5ca303700bccc67d8a62f
naodmulu <100387620+naodmulu@users.noreply.github.com>
2022-03-11 23:50:53 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,11 @@ int main(void)
 	char ch;
 	for (ch = 'a'; ch <= 'z'; ch++ )
 	{
-		if (ch != 'e' && ch != 'q')
+		if (ch == 'e' || ch == 'q')
 		{
 			continue ;
 		}
+		putchar(ch)
 	}
 	putchar('\n');
 	return (0);
