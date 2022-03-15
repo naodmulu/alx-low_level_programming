1b204c4554997e0bc32815bf005289a99bb9672b
naodmulu <naodmulugeta1621@gmail.com>
2022-03-15 16:53:07 +0300

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
  * print_alphabet_x10 - prints alphabet, in lower case, followed by new line, ten times
  * Return: null
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	char ch;
 	for (int x = 1; x <= 10; x++)
